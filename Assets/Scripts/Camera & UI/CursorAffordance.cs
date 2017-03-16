7fd8a7c5cfb0798a1aed4050d62d398722358d81
Menti <josementi@gmail.com>
2017-03-16 20:23:21 +0000

    Cursor affordance and project tidy up

---
+++ Assets/Scripts/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CursorAffordance : MonoBehaviour {
+    
+    [SerializeField]
+    CameraRaycaster raycaster;
+
+    [SerializeField]
+    Texture2D walkCursor = null;
+    [SerializeField]
+    Texture2D enemyCursor = null;
+    [SerializeField]
+    Texture2D errorCursor = null;
+
+    [SerializeField]
+    Vector2 cursorHotSpot = new Vector2(96, 96);
+
+	// Use this for initialization
+	void Start () {
+        Assert.IsNotNull(raycaster);
+        Assert.IsNotNull(walkCursor);
+        Assert.IsNotNull(enemyCursor);
+        Assert.IsNotNull(errorCursor);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (raycaster.layerHit)
+        {
+            case Layer.Enemy:
+                Cursor.SetCursor(enemyCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            case Layer.Walkable:
+                Cursor.SetCursor(walkCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+            case Layer.RaycastEndStop:
+                Cursor.SetCursor(errorCursor, cursorHotSpot, CursorMode.Auto);
+                break;
+        }
+	}
+}
